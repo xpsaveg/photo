c3e02fbea9c04b7c41078eabeef7d4b8f08d5dae
xpsaveg <128470474+xpsaveg@users.noreply.github.com>
2023-05-15 15:49:58 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import request
+from flask import Flask, render_template, request, url_for
+from werkzeug.utils import redirect
 from flask import Flask,render_template,request
 from werkzeug.utils import redirect
 from flask_sqlalchemy import SQLAlchemy
@@ -10,29 +12,25 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 app = Flask(__name__)
 
+# data base
 app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 with app.app_context():
     class User(UserMixin, db.Model):
-        _tablename_ = 'users'
+        __tablename__ = 'User'
         id = db.Column(db.Integer, primary_key=True)
-        phone = db.Column(db.String(100), unique=True)
+        name = db.Column(db.String(100), unique=True)
         password = db.Column(db.String(100))
-        name = db.Column(db.String(1000))
-
-
-    class Messages(UserMixin, db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        link = db.Column(db.String(2000))
-        image = db.Column(db.String(2000))
-        description = db.Column(db.String(4000))
-
-
+        phone = db.Column(db.String(100))
+        city = db.Column(db.String(1000))
 
     db.session.commit()
     db.create_all()
+
+
+
 class MyModelView(ModelView):
     def is_accessible(self):
 
@@ -40,66 +38,34 @@ class MyModelView(ModelView):
 
 admin = Admin(app)
 admin.add_view(MyModelView(User, db.session))
-admin.add_view(MyModelView(Messages, db.session))
 
 
-@app.route("/",methods=["GET","POST"])
+
+@app.route("/", methods=["GET", "POST"])
+def add():
+    user=User.query.all()
+    return render_template("login.html")
+
+
+     # return render_template("register.html")
+@app.route("/register",methods=["GET","POST"])
 def register():
     if request.method=="POST":
-        name=request.form.get("name")
-        password=request.form.get("password")
-        phone=request.form.get("password")
+        name= request.form.get("name")
+        password= request.form.get("password")
+        city= request.form.get("city")
+        phone = request.form.get("phone")
         new=User(
             name=name,
             password=password,
-            phone=password,
+            city=city,
+            phone=phone,
 
         )
+
         db.session.add(new)
         db.session.commit()
 
     return render_template("register.html")
-
-data={
-    "img":"https://images.unsplash.com/photo-1661956601031-4cf09efadfce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80",
-    "i": "https://images.unsplash.com/photo-1661956601031-4cf09efadfce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80",
-
-}
-
-rating={
-    "img":"good",
-    # "i":2
-}
-
-
-
-@app.route("/")
-def start():
-    images=data
-    return render_template("lessson5.html",img=images)
-@app.route("/add", methods=["GET", "POST"])
-def add():
-    if request.method == "POST":
-        name = request.form.get("name")
-        link = request.form.get("link")
-        data[name] = link
-        return redirect("/")
-    return render_template("add.html")
-
-@app.route("/show")
-def show():
-    key = request.args.get('key')
-    for k in rating:
-        if k==key:
-            return f"{rating[key]}"
-
-    return f" i didnot find a rating for {key}"
-
-
 if __name__==("__main__"):
-    app.run(debug=True)
-
-
-if __name__==("__main__"):
-
     app.run(debug=True)
